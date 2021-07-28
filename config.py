528d0b76049fd22b61195a06b805ce9b34bcd22f
Paul <55580385+Anime-pdf@users.noreply.github.com>
2021-07-28 17:41:31 +0300

    init

---
+++ config.py
@@ -0,0 +1,9 @@
+token = "NzQwNTc1MjQ2NTA3MTgwMDYy.XyrAcQ.RaynRA9lD7FQKhos1_aAl18YC2k"
+
+guild_id = 704602626721054771
+channel_id = 868867988630143038
+message_id = 868868143207034941
+
+adminchannel_id = 868867986046459944
+
+emoji = "🎫"
